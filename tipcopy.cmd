b281cf53b18046ceae6533ec9ede5f91ffba3658
epocman <epocman@3b780cf9-a0bc-42d8-8050-8c2e19917c16>
2006-02-28 16:42:42 +0000

    Preparing for the 2.5 development loop
    git-svn-id: svn://svn.code.sf.net/p/ibpp/code/trunk@6 3b780cf9-a0bc-42d8-8050-8c2e19917c16

--- tipcopy.cmd
+++
@@ -1,9 +0,0 @@
-copy /Y ibpp.h L:\AddOns\IBPP
-REM
-copy /Y release\win32_msvc\ibpp.lib L:\AddOns\IBPP
-copy /Y debug\win32_msvc\ibppd.lib L:\AddOns\IBPP
-copy /Y debug\win32_msvc\ibppd.pdb L:\AddOns\IBPP
-copy /Y tests\release\win32_msvc\tests.exe L:\AddOns\IBPP
-REM
-copy /Y release\win32_bcc\ibpp.lib L:\AddOns\IBPP\ibpp_b.lib
-copy /Y debug\win32_bcc\ibppd.lib L:\AddOns\IBPP\ibpp_bd.lib
